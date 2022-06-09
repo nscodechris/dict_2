cbcafeee7c31e8b1515509bef2377d112d907aa6
nscodechris <christoffer.vdmab@gmail.com>
2022-06-09 15:40:50 +0200

    replace if

--- dict.py
+++ dict.py
@@ -28,7 +28,10 @@ def save_dict(conn):
 while True: ## REPL - Read Execute Program Loop
     cmd = input("Command: ")
     if cmd == "LIST":
-        print(read_dict(conn))
+        if cmd == "list":
+            for i, wd, trans in read_dict(conn):
+                print(f"{i}: {wd} - {trans}")
+
     elif cmd == "ADD":
         name = input("  Word: ")
         phone = input("  Translation: ")
